0d99bf74c88f64e0b69fc0036aad825dfc52d7cb
Simon Nonnis <simon.nonnis@protonmail.com>
2018-08-17 09:57:54 +0100

    :art: Add more columns

--- src/initial-data.js
+++ src/initial-data.js
@@ -4,6 +4,8 @@ const TASK3 = 'task3';
 const TASK4 = 'task4';
 
 const COLUMN1 = 'column1';
+const COLUMN2 = 'column2';
+const COLUMN3 = 'column3';
 
 const initialData = {
   tasks: {
@@ -17,9 +19,19 @@ const initialData = {
       id: COLUMN1,
       title: 'To Do',
       taskIDs: [TASK1, TASK2, TASK3, TASK4]
+    },
+    [COLUMN2]: {
+      id: COLUMN2,
+      title: 'In progress',
+      taskIDs: []
+    },
+    [COLUMN3]: {
+      id: COLUMN3,
+      title: 'Done',
+      taskIDs: []
     }
   },
-  columnOrder: [COLUMN1]
+  columnOrder: [COLUMN1, COLUMN2, COLUMN3]
 };
 
 export default initialData;
